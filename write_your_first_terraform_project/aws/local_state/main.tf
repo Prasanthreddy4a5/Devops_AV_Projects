1e483fd09473c724c0838736ada058ce4254a59b
Prasanthreddy4a5 <ummadi1994@gmail.com>
2023-11-25 17:21:54 +0530

    nc

--- write_your_first_terraform_project/aws/local_state/main.tf
+++ write_your_first_terraform_project/aws/local_state/main.tf
@@ -13,12 +13,12 @@ provider "aws" {
   region  = "us-west-1"
 }
 
-resource "aws_instance" "devops-practice" {
-  ami           = "ami-081609eef2e3cc958"
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "Terraform_Demo_Practice"
+    Name = "Terraform_Demo"
   }
 }
 #
