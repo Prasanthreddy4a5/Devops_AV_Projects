2a8bc86a525828ecedb1b50db6f0c86e1a91e54a
Prasanthreddy4a5 <ummadi1994@gmail.com>
2023-11-25 17:18:56 +0530

    afas

--- write_your_first_terraform_project/aws/local_state/main.tf
+++ write_your_first_terraform_project/aws/local_state/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
   region  = "us-west-1"
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_instance" "devops-practice" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
 
