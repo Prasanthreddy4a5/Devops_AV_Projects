f0474d778c4b1557b35ee9d5a378e1b8b49c2fb1
Prasanthreddy4a5 <ummadi1994@gmail.com>
2023-12-18 21:46:56 +0530

    resource

--- write_your_first_terraform_project/aws/local_state/main.tf
+++ write_your_first_terraform_project/aws/local_state/main.tf
@@ -23,16 +23,14 @@ resource "aws_instance" "app_server" {
 }
 
 
-provider "aws" {
-  region  = "us-east-1"
-}
-  
-resource "aws_instance" "devops_practice" {
+
+
+
+resource "aws_instance" "web" {
   ami           = "ami-03265a0778a880afb"
-  instance_type = "t2.micro"
-  
+  instance_type = "t3.micro"
 
   tags = {
-    Name = "devops-practice"
+    Name = "HelloWorld"
   }
 }
--- write_your_first_terraform_project/aws/test/test.tf
+++ write_your_first_terraform_project/aws/test/test.tf
@@ -9,3 +9,13 @@ output "Hello" {
     value = "Hello India"
     
 }
+
+
+resource "aws_instance" "web" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
