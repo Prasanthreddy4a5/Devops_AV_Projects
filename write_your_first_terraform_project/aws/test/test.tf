fd79a39f02dfdeb15cd4c1e9655eb3363b1a43dd
Prasanthreddy4a5 <ummadiprashanthreddy@gmail.com>
2023-12-22 12:25:05 +0530

    re

--- write_your_first_terraform_project/aws/test/test.tf
+++ write_your_first_terraform_project/aws/test/test.tf
@@ -22,18 +22,6 @@ value = var.fruit
 }
 
 
-variable "components" {
-  default = ["catalogue", "frontend", "mongodb"]
-}
-
-  #count = length(var.components)
-
-output "components"{
-value = var.components[count.index]
-}
-
-  #Name = var.components[count.index]
-  #Name = element(var.components, count.index)
   
 
 
